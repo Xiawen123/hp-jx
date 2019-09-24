dde84f0d45cd43343ae09bcaf69b6fce2f656cf7
xjx <774737421@qq.com>
2019-09-24 15:03:44 +0800

    乔

--- hp-admin/src/main/java/com/hp/web/controller/XiaJinxin.java
+++ hp-admin/src/main/java/com/hp/web/controller/XiaJinxin.java
@@ -4,10 +4,10 @@ package com.hp.web.controller;
  * @author: 夏金鑫
  * @program: hp-jx
  * @description: ${description}
- * @create: 2019/09/24 14:37
+ * @create: 2019/09/24 15:03
  **/
 public class XiaJinxin {
     public static void main(String[] args) {
-        System.out.println(1231313);
+        System.out.println(32132132);
     }
 }
