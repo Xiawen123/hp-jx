8b93206fb929fa42c834908dc733bd1b176b3931
xjx <774737421@qq.com>
2019-09-24 14:45:30 +0800

    1231321

---
+++ hp-admin/src/main/java/com/hp/web/controller/XiaJinxin.java
@@ -0,0 +1,13 @@
+package com.hp.web.controller;
+
+/**
+ * @author: 夏金鑫
+ * @program: hp-jx
+ * @description: ${description}
+ * @create: 2019/09/24 14:37
+ **/
+public class XiaJinxin {
+    public static void main(String[] args) {
+
+    }
+}
